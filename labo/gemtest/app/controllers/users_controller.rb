be61e0ca63ff9b95bb22b90e2376c530f583c06b
tarofuji6974 <amazarashi2525@icloud.com>
2019-12-04 21:41:41 +0900

    kaminariによるユーザー一覧のページネーションを実装

--- labo/gemtest/app/controllers/users_controller.rb
+++ labo/gemtest/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  #ページネーション
+  PER = 10
+
   def index
-    @users = User.all
+    #@users = User.all
+    @users = User.page(params[:page]).per(PER)
   end
 
   def show
