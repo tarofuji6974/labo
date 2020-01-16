b197156d4ed73c7d424e4ad17414e8d785e36e38
tarofuji6974 <amazarashi2525@icloud.com>
2020-01-16 23:48:06 +0900

    編集機能/一覧表示機能の実装

--- labo/Tscaffold/app/controllers/theme_controller.rb
+++ labo/Tscaffold/app/controllers/theme_controller.rb
@@ -1,23 +1,32 @@
 class ThemeController < ApplicationController
-
-  #トップ画面(ログイン)
-  def top
-    if current_user
-      @user = User.find(current_user.id)
-
-      @theme = Theme.where(user_id: @user.id).order(created_at: :asc)
-    else
-      redirect_to("/users/sign_in")
-    end
-  end
+  PER = 10  #投稿の表示数
 
   #お題の閲覧
   def view
+     #ransack
+    @q = Theme.ransack(params[:q])
+
+    #作成日時の降順になるようソート
+    @q.sorts   = 'created_at desc' if @q.sorts.empty?
+
+    @ask = @q.result(distinct: true)
+
+    #ページング(作成日時の降順に表示させるよう修正の必要あり)
+    @themes = Theme.page(params[:page]).per(PER)
+
   end
 
   #お題の作成ページ表示
   def create
     @user = User.find(params[:id])
+
+    if @user.name ==nil
+      flash[:notice] = '名前の入力をお願いします。設定が完了するとお題の投稿が可能になります'
+      redirect_to("/profile_edit/#{@user.id}")
+
+      return
+    end
+
     @theme = Theme.new
   end
 
@@ -50,18 +59,50 @@ class ThemeController < ApplicationController
   end
 
   def profile
+    if current_user
+      @user = User.find(current_user.id)
+
+      @theme = Theme.where(user_id: @user.id).order(created_at: :asc)
+
+    else
+      redirect_to("/users/sign_in")
+    end
   end
 
   #プロフィールの編集
   def profile_edit
+    @user = User.find(params[:id])
 
+    if @user.name == nil
+      flash[:notice] = '名前の入力をお願いします。設定が完了するとお題の投稿が可能になります'
+    end
+  end
+
+  def profile_update
+    user =  User.find(params[:id])
+    user.name = params[:name]
+    user.profile = params[:profile]
+
+    if user.save
+      flash[:notice] = "編集が完了しました"
+      redirect_to(root_path)
+
+    else
+      render("/profile_edit/#{user.id}")
+    end
   end
 
   #コメントの追加
   def comment
+
   end
 
   #お題の削除
   def delete
+    theme = Theme.find_by(id: params[:id])
+    theme.destroy
+
+    flash[:notice] = "お題を削除しました"
+    redirect_to(root_path)
   end
 end
--- labo/Tscaffold/config/routes.rb
+++ labo/Tscaffold/config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
 
   resources :theme, :replace_id_with => 'url'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'theme#top'
+  root 'theme#profile'
 
   get 'create/:id' => 'theme#create'
   post 'create_theme' => 'theme#create_theme'
   get 'profile/:id' => 'theme#profile'
   get 'profile_edit/:id' => 'theme#profile_edit'
+  post 'profile_update/:id' => 'theme#profile_update'
   get 'view' => 'theme#view'
+  get 'theme_edit/:id/:url' => 'theme#theme_edit'
+  post 'delete/:id' => 'theme#delete'
+
 end
